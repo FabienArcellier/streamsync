9b7c91aff1f8d8cb16852a78d2391067abe81200
Fabien Arcellier <fabien.arcellier@gmail.com>
2024-02-26 09:25:01 +0100

    chore: add documentation build to continuous integration process

--- alfred/ci.py
+++ alfred/ci.py
@@ -6,13 +6,19 @@ import alfred
 @alfred.command("ci", help="continuous integration pipeline")
 @alfred.option('--front', '-f', help="run for frontend only", is_flag=True, default=False)
 @alfred.option('--back', '-b', help="run for backend only", is_flag=True, default=False)
-def ci(front, back):
-    if back or (not front and not back):
+@alfred.option('--docs', '-d', help="run for documentation only", is_flag=True, default=False)
+def ci(front, back, docs):
+    run_all = (not front and not back and not docs)
+    if back or run_all:
         alfred.invoke_command("ci.mypy")
         alfred.invoke_command("ci.pytest")
-    if front or (not front and not back):
-        alfred.invoke_command("npm.lint")
-        alfred.invoke_command("npm.build")
+
+    if front or run_all:
+        alfred.invoke_command("npm.ui.lint")
+        alfred.invoke_command("npm.ui.build")
+
+    if docs or run_all:
+        alfred.invoke_command("npm.docs.build")
 
 @alfred.command("ci.mypy", help="typing checking with mypy on ./src/streamsync")
 def ci_mypy():
--- alfred/npm.py
+++ alfred/npm.py
@@ -1,17 +1,23 @@
 import alfred
 import os
 
-@alfred.command("npm.lint", help="lint check ui code")
-def npm_lint():
+
+@alfred.command("npm.docs.build", help="build streamsync website and documentation")
+def npm_docs_build():
+    os.chdir("docs")
+    alfred.run("npm run docs:build")
+
+@alfred.command("npm.ui.lint", help="lint check ui code")
+def npm_ui_lint():
     os.chdir("ui")
     alfred.run("npm run lint:ci")
 
-@alfred.command("npm.build", help="build ui code")
-def npm_build():
+@alfred.command("npm.ui.build", help="build ui code")
+def npm_ui_build():
     os.chdir("ui")
     alfred.run("npm run build")
 
-@alfred.command("npm.build_custom_components", help="build custom components")
-def ui_build_custom():
+@alfred.command("npm.ui.build_custom_components", help="build custom components")
+def npm_ui_build_custom():
     os.chdir("ui")
     alfred.run("npm run custom.build")
